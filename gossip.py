9c1f996d230e1be45d32ddc3b4927059a2c16f7d
babyldm <babyldm@outlook.COM>
2017-08-04 08:26:15 +0800

    Update gossip.py

--- gossip.py
+++ gossip.py
@@ -16,25 +16,21 @@
 import sys
 import numpy
 from keras.callbacks import TensorBoard, ModelCheckpoint
-
-from windpuller import WindPuller
+import pandas as pd
+from windpuller1 import WindPuller
 from dataset import DataSet
-from feature import extract_from_file
+from feature1 import extract_from_file
 
-
-def read_ultimate(path, input_shape):
-    ultimate_features = numpy.loadtxt(path + "ultimate_feature." + str(input_shape[0]))
-    ultimate_features = numpy.reshape(ultimate_features, [-1, input_shape[0], input_shape[1]])
-    ultimate_labels = numpy.loadtxt(path + "ultimate_label." + str(input_shape[0]))
-    # ultimate_labels = numpy.reshape(ultimate_labels, [-1, 1])
+def read_ultimate(path, input_shape,code="000001"):
+    ultimate_features = pd.read_csv(path +code+ "_feature." + str(input_shape[0])).values
+    ultimate_features = ultimate_features.reshape(-1, input_shape[0], input_shape[1])
+    ultimate_labels =  pd.read_csv(path +code+ "_label." + str(input_shape[0])).values
     train_set = DataSet(ultimate_features, ultimate_labels)
-    test_features = numpy.loadtxt(path + "ultimate_feature.test." + str(input_shape[0]))
-    test_features = numpy.reshape(test_features, [-1, input_shape[0], input_shape[1]])
-    test_labels = numpy.loadtxt(path + "ultimate_label.test." + str(input_shape[0]))
-    # test_labels = numpy.reshape(test_labels, [-1, 1])
+    test_features = pd.read_csv(path + code+"_feature.test." + str(input_shape[0])).values
+    test_features = test_features.reshape(-1, input_shape[0], input_shape[1])
+    test_labels = pd.read_csv(path +code+ "_label.test." + str(input_shape[0])).values
     test_set = DataSet(test_features, test_labels)
     return train_set, test_set
-
 
 def read_feature(path, input_shape, prefix):
     ultimate_features = numpy.loadtxt("%s/%s_feature.%s" % (path, prefix, str(input_shape[0])))
@@ -50,16 +46,15 @@ def read_feature(path, input_shape, prefix):
     return train_set, test_set
 '''
 def read_ultimate(path, input_shape):
-    ultimate_features = numpy.loadtxt(path + "ultimate_feature." + str(input_shape[0]))
-    ultimate_features = numpy.reshape(ultimate_features, [-1, input_shape[0], input_shape[1]])
-    ultimate_labels = numpy.loadtxt(path + "ultimate_label." + str(input_shape[0]))
+    ultimate_features = pd.read_csv(path + "ultimate_feature." + str(input_shape[0])).values
+    ultimate_features = ultimate_features.reshape(-1, input_shape[0], input_shape[1])
+    ultimate_labels =  pd.read_csv(path + "ultimate_label." + str(input_shape[0])).values
     train_set = DataSet(ultimate_features[:len(ultimate_features) * 9 // 10, :, :],
-                        ultimate_labels[:len(ultimate_labels) * 9 // 10])
+                       ultimate_labels[:len(ultimate_labels) * 9 // 10])
     test_set = DataSet(ultimate_features[len(ultimate_features) * 9 // 10:, :, :],
-                       ultimate_labels[len(ultimate_labels) * 9 // 10:])
+                      ultimate_labels[len(ultimate_labels) * 9 // 10:])
     return train_set, test_set
 '''
-
 
 def calculate_cumulative_return(labels, pred):
     cr = []
@@ -75,7 +70,7 @@ def calculate_cumulative_return(labels, pred):
 
 def evaluate_model(model_path, code, input_shape=[30, 61]):
     extract_from_file("dataset/%s.csv" % code, code)
-    train_set, test_set = read_feature(".", input_shape, code)
+    train_set, test_set =read_ultimate("./", input_shape,code=code)
     saved_wp = WindPuller(input_shape).load_model(model_path)
     scores = saved_wp.evaluate(test_set.images, test_set.labels, verbose=0)
     print('Test loss:', scores[0])
@@ -85,13 +80,12 @@ def evaluate_model(model_path, code, input_shape=[30, 61]):
     print("changeRate\tpositionAdvice\tprincipal\tcumulativeReturn")
     for i in range(len(test_set.labels)):
         print(str(test_set.labels[i]) + "\t" + str(pred[i]) + "\t" + str(cr[i] + 1.) + "\t" + str(cr[i]))
-
-
-def make_model(input_shape, nb_epochs=100, batch_size=128, lr=0.01, n_layers=1, n_hidden=16, rate_dropout=0.3):
+def make_model(input_shape, nb_epochs=100, batch_size=128, lr=0.01, n_layers=1, n_hidden=16, rate_dropout=0.3,code="000001"):
     model_path = 'model.%s' % input_shape[0]
     wp = WindPuller(input_shape=input_shape, lr=lr, n_layers=n_layers, n_hidden=n_hidden, rate_dropout=rate_dropout)
-    train_set, test_set = read_ultimate("./", input_shape)
-    wp.fit(train_set.images, train_set.labels, batch_size=batch_size,
+    train_set, test_set = read_ultimate("./", input_shape,code=code)
+
+    wp.fit(train_set.images, numpy.squeeze(train_set.labels), batch_size=batch_size,
            nb_epoch=nb_epochs, shuffle=True, verbose=1,
            validation_data=(test_set.images, test_set.labels),
            callbacks=[TensorBoard(histogram_freq=1),
@@ -99,29 +93,25 @@ def make_model(input_shape, nb_epochs=100, batch_size=128, lr=0.01, n_layers=1, 
     scores = wp.evaluate(test_set.images, test_set.labels, verbose=0)
     print('Test loss:', scores[0])
     print('Test accuracy:', scores[1])
-
     wp.model.save(model_path)
     saved_wp = wp.load_model(model_path)
     scores = saved_wp.evaluate(test_set.images, test_set.labels, verbose=0)
     print('Test loss:', scores[0])
     print('test accuracy:', scores[1])
     pred = saved_wp.predict(test_set.images, 1024)
-    # print(pred)
-    # print(test_set.labels)
     pred = numpy.reshape(pred, [-1])
-    result = numpy.array([pred, test_set.labels]).transpose()
-    with open('output.' + str(input_shape[0]), 'w') as fp:
-        for i in range(result.shape[0]):
-            for val in result[i]:
-                fp.write(str(val) + "\t")
-            fp.write('\n')
+    pred=numpy.array(pred)
+    labels=numpy.squeeze(test_set.labels)
+    result = numpy.array([pred, labels]).transpose()
+    reslut=pd.DataFrame(result)
+    reslut.to_csv('output.' + str(input_shape[0]), header=None, index=False)
 
 if __name__ == '__main__':
-    operation = "train"
+    operation = "predict"
     if len(sys.argv) > 1:
         operation = sys.argv[1]
     if operation == "train":
-        make_model([30, 61], 3000, 512, lr=0.001)
+        make_model([30, 61], 1, 512, lr=0.002,code="000001")
     elif operation == "predict":
         evaluate_model("model.30.best", "000001")
     else:
